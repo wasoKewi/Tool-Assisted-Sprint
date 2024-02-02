80e393135e91be80b0b8b9cb36aa551c8d674941
Mizhi <117398705+wasoKewi@users.noreply.github.com>
2024-02-02 01:54:18 +0000

    Button Stop
    Have it disable the buttons when confirmed, dissalow new button press after confirm, make it wait, reenable.

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -9,21 +9,20 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if get_node("../../UI").fullconf:
+		if not is_on_floor():
+			velocity.y += gravity * delta
 
-	# Handle jump.
-	if get_node("../../UI").jump2 and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Handle jump.
+		if get_node("../../UI").jump2 and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = get_node("../../UI").xdir2
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		var direction = get_node("../../UI").xdir2
+		
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		get_node("../../UI").turndone = true
 	move_and_slide()
